a7133476e4eef92d98387716c53aa6f69ff97d5b
Stefan von Ellenrieder <stefanmve@gmail.com>
2015-03-30 20:09:33 -0500

    Revert bec8ae1..cbce927
    This rolls back to commit bec8ae1122c31bef8f3819bdba372f1b18649049.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,10 @@
 'use strict';
 
 var container = document.getElementById('container');
-var winWidth = window.innerWidth;
+var winWidth = window.innerWidth - getScrollerWidth();
+
+// var winWidth = container.getBoundingClientRect();
+//console.log(winWidth.width)
 var tiles = document.getElementsByClassName('tile');
 var tileCopy = [];
 var tilesLength = tiles.length;
@@ -51,27 +54,27 @@ for (var i = 0; i < tilesLength; i++) {
 	tiles[i].innerHTML = '<div class=\'content\'>' + tiles[i].innerHTML + '</div>';
 	tileCopy.push([tiles[i].outerHTML, size]);
 }
-console.log('totalArea: ' + totalArea);
+//console.log('totalArea: ' + totalArea);
 //console.log('tileCopy: ' + tileCopy);
 
 
 remainder = totalArea % col;
-console.log('r: ' + remainder);
+//console.log('r: ' + remainder);
 
 if (remainder !== 0) {
 	filler = col * 2 - remainder;
 }
 totalTiles += filler;
-console.log('f: ' + filler);
-console.log('totalTiles: ' + totalTiles);
+//console.log('f: ' + filler);
+//console.log('totalTiles: ' + totalTiles);
 
 for (var i = tilesLength; i < totalTiles; i++) {
 	var size = 'filler';
 	var area = tileSizes[size][0] * tileSizes[size][1];
-	tileCopy.push(['<div class=\'tile filler\'><div class=\'content\'><p>Filler<p></div></div>',size]);
+	tileCopy.push(['<div class=\'tile filler\'><div class=\'content\'></div></div>',size]);
 	totalArea += area;
 }
-console.log('totalArea ' + totalArea);
+//console.log('totalArea ' + totalArea);
 var rows = totalArea / col;
 
 while (container.hasChildNodes()) {
@@ -87,20 +90,52 @@ var last = 0;
 var nextXCoord = 0;
 var nextYCoord = 0;
 for (var i = 0; i < tileCopy.length; i++) {
-	console.log(i + 1 + " of " + tileCopy.length);
+	//console.log(i + 1 + " of " + tileCopy.length);
 	var div = document.createElement('div');
 	var size = tileCopy[i][1];
+	//console.log(size);
 	var unitWidth = winWidth / col;
 	div.innerHTML = tileCopy[i][0];
 	var insert = div.firstChild;
-	var coords = griddy.find(insert, size);
-	console.log(coords);
-	docfrag.appendChild(insert);
-	//position(insert, coords[x], coords[y]);
-	insert.setAttribute('style', 'left:' + coords['x'] * unitWidth + 'px; top:' + coords['y'] * unitWidth +
-	'px;');
+	if (size !== 'filler') {
+		var coords = griddy.find(insert, size);
+		//console.log(coords);
+		docfrag.appendChild(insert);
+		//position(insert, coords[x], coords[y]);
+		insert.setAttribute('style', 'left:' + coords['x'] * unitWidth + 'px; top:' + coords['y'] * unitWidth +
+		'px;');
+	}
 	
 	
+	
+	// if (nextXCoord >= winWidth) {
+	// 	nextYCoord = winWidth / col;
+	// 	nextXCoord = 0;
+	// }
+}
+
+
+
+
+for (var i = 0; i < tileCopy.length; i++) {
+	//console.log(i + 1 + " of " + tileCopy.length);
+	var div = document.createElement('div');
+	var size = tileCopy[i][1];
+	//console.log(size);
+	var unitWidth = winWidth / col;
+	div.innerHTML = tileCopy[i][0];
+	var insert = div.firstChild;
+	if (size === 'filler') {
+		var coords = griddy.find(insert, size);
+		//console.log(coords);
+		docfrag.appendChild(insert);
+		//position(insert, coords[x], coords[y]);
+		insert.setAttribute('style', 'left:' + coords['x'] * unitWidth + 'px; top:' + coords['y'] * unitWidth +
+		'px;');
+	}
+
+
+
 	// if (nextXCoord >= winWidth) {
 	// 	nextYCoord = winWidth / col;
 	// 	nextXCoord = 0;
@@ -134,12 +169,13 @@ function Grid(col, rows) {
 		};
 		
 		this.find = function(obj, size) {
+			//console.log('finding: ' + size);
 			var dimensions = tileSizes[size];
 			
 			if (dimensions[0] === 1 && dimensions[1] === 1) {
 				for (var i=0; i < col*rows; i++) {
 					if (this.grid[i] === 0) {
-						this.grid[i] = obj;
+						this.grid[i] = size;
 						return this.place(i);
 					}	
 				}
@@ -147,19 +183,20 @@ function Grid(col, rows) {
 			if (dimensions[0] === 2 && dimensions[1] === 1) {
 				for (var i=0; i < col*rows; i++) {
 					if (this.grid[i] === 0 && this.grid[i+1] === 0 && (i + 1)%col !== 0) {
-						this.grid[i] = obj;
-						this.grid[i+1] = obj;
+						this.grid[i] = size;
+						this.grid[i+1] = size;
 						return this.place(i);
 					}	
 				}
 			}
 			if (dimensions[0] === 1 && dimensions[1] === 2) {
-				for (var i=0; i < col*rows; i++) {
+				for (var i=0; i < (col*rows)+ col; i++) {
+					//console.log('i: ' + i);
 					if (this.grid[i] === 0 && this.grid[i+col] === 0) {
-						this.grid[i] = obj;
-						this.grid[i+col] = obj;
+						this.grid[i] = size;
+						this.grid[i+col] = size;
 						return this.place(i);
-					}	
+					} 	
 				}
 			}
 			
@@ -177,7 +214,48 @@ function Grid(col, rows) {
 				var y = Math.ceil(input/col)-1;
 				var x = input%col;
 			}
-			
+			//console.log('x: ' + x + ', y: ' + y + ', index: ' + input);
 			return {x: x, y:y, index:input};
 		};
+	}
+	
+	function getScrollerWidth() {
+		var scr = null;
+		var inn = null;
+		var wNoScroll = 0;
+		var wScroll = 0;
+	
+		// Outer scrolling div
+		scr = document.createElement('div');
+		scr.style.position = 'absolute';
+		scr.style.top = '-1000px';
+		scr.style.left = '-1000px';
+		scr.style.width = '100px';
+		scr.style.height = '50px';
+		// Start with no scrollbar
+		scr.style.overflow = 'hidden';
+	
+		// Inner content div
+		inn = document.createElement('div');
+		inn.style.width = '100%';
+		inn.style.height = '200px';
+	
+		// Put the inner div in the scrolling div
+		scr.appendChild(inn);
+		// Append the scrolling div to the doc
+		document.body.appendChild(scr);
+	
+		// Width of the inner div sans scrollbar
+		wNoScroll = inn.offsetWidth;
+		// Add the scrollbar
+		scr.style.overflow = 'auto';
+		// Width of the inner div width scrollbar
+		wScroll = inn.offsetWidth;
+	
+		// Remove the scrolling div from the doc
+		document.body.removeChild(
+			document.body.lastChild);
+	
+		// Pixel width of the scroller
+		return (wNoScroll - wScroll);
 	}
